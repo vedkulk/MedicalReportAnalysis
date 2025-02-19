d1f0b92a71a6369152a70df3a6edadca0397a10c
vedkulk <kulkarnived3@gmail.com>
2025-02-19 23:11:32 +0530

    frontend

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import VectorDBpage from "./pinecone/page";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center p-24">
+      <VectorDBpage></VectorDBpage>
+    </div>
+  );
+}
---
+++ app/pinecone/page.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from '@/components/ui/textarea';
+import * as LucideIcons from 'lucide-react';
+import React, { useState } from 'react'
+
+type Props = {}
+
+const VectorDBpage = (props: Props) => {
+  const [isUploading, setisUploading] = useState(false)
+  return (
+    <main className='flex flex-col items-center p-24'>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Update Knowledge Base</CardTitle>
+                    <CardDescription>Add new docuemnts to your vector DB</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className='grid grid-cols-3 gap-4'>
+                        <div className='col-span-2 grid gap-4 border rounded-lg p-6'>
+                            <div className='gap-4 relative'>
+                                <Button  className='absolute -right-4 -top-4' variant={'ghost'} size={'icon'}>
+                                    <LucideIcons.RefreshCcw />
+                                </Button>
+                                <Label>Files List:</Label>
+                                <Textarea readOnly
+                                    className='min-h-24 resize-none border p-3 shadow-none disabled:cursor-default focus-visible:ring-0 text-sm text-muted-foreground'
+                                />
+                            </div>
+                            <div className='grid grid-cols-2 gap-4'>
+                                <div className="grid gap-2">
+                                    <Label>
+                                        Index Name
+                                    </Label>
+                                    <Input placeholder='index name' disabled={isUploading}  className='disabled:cursor-default' />
+                                </div>
+                                <div className="grid gap-2">
+                                    <Label>
+                                        Namespace
+                                    </Label>
+                                    <Input placeholder='namespace' disabled={isUploading}  className='disabled:cursor-default' />
+                                </div>
+                            </div>
+                        </div>
+                        <Button  variant={'outline'} className='w-full h-full' disabled={isUploading} >
+                            <span className='flex flex-row'>
+                                <LucideIcons.Database size={50} className='stroke-[#D90013]' />
+                                <LucideIcons.MoveUp className='stroke-[#D90013]' />
+                            </span>
+                        </Button>
+                    </div>
+                     <div className='mt-4'>
+                        <Label>File Name: </Label>
+                        <div className='flex flex-row items-center gap-4'>
+                            <Progress  value={80}/>
+                            <LucideIcons.LucideLoader2 className='stroke-[#D90013] animate-spin' />
+                        </div>
+                    </div>
+                </CardContent>
+            </Card>
+        </main>
+    )
+}
+export default VectorDBpage
